339b8b00bff4038c048cad8f8ff39c22fb198d65
Kacper Jach <kacperini@gmail.com>
2022-06-11 20:28:34 +0200

    Delegacje added

---
+++ Delegacje/Delegacje/Delegaty.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegacje
+{
+    class Delegaty
+    {
+        public int max(int arg1, int arg2, int arg3, int arg4, int arg5)
+        {
+            int[] arr = new int[] { arg1, arg2, arg3, arg4, arg5 };
+            int max = arr.Max();
+            return max;
+        }
+        public int min(int arg1, int arg2, int arg3, int arg4, int arg5)
+        {
+            int[] arr = new int[] { arg1, arg2, arg3, arg4, arg5 };
+            int min = arr.Min();
+            return min;
+        }
+        public int avg(int arg1, int arg2, int arg3, int arg4, int arg5)
+        {
+            int[] arr = new int[] { arg1, arg2, arg3, arg4, arg5 };
+            int avg = arr.Sum()/5;
+            return avg;
+        }
+        public int median(int arg1, int arg2, int arg3, int arg4, int arg5)
+        {
+            int[] arr = new int[] { arg1, arg2, arg3, arg4, arg5 };
+            Array.Sort(arr);
+            if(arr.Length % 2 == 0)
+            {
+                return (arr[arr.Length / 2 - 1] + arr[arr.Length / 2]) / 2;
+            } 
+            else
+            {
+                return arr[(arr.Length - 1) / 2];
+            }
+        }
+    }
+}
---
+++ Delegacje/Delegacje/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Delegacje
+{
+    class Program
+    {
+        public delegate int Delegate(int arg1, int arg2, int arg3, int arg4, int arg5);
+        static void Main(string[] args)
+        {
+            
+            Delegaty delegat = new Delegaty();
+
+            Delegate del = delegat.max;
+            Console.WriteLine(del(10, 12, 94, 1, 213));
+
+            del += delegat.min;
+            Console.WriteLine(del(10, 12, 94, 1, 213));
+
+            del += delegat.avg;
+            Console.WriteLine(del(10, 12, 94, 1, 213));
+
+            del += delegat.median;
+            Console.WriteLine(del(10, 12, 94, 1, 213));
+        }
+    }
+}
