69ee9d76d7d780cb0b0e4486ad17caba885543fa
Kacper Jach <kacperini@gmail.com>
2022-06-12 13:18:45 +0200

    klasy zagniezdzone done

---
+++ Klasy-Zagniezdzone/Klasy-Zagniezdzone/Outside.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasy_Zagniezdzone
+{
+    class Outside
+    {
+        public class Inside
+        {
+            public double NewtonRaphson(double n)
+            {
+                double e = 0.000001;
+                double x = n / 2;
+                while (Math.Abs(x - n / x) > e)
+                {
+                    x = (x + n / x) / 2;
+                    if (x * x == n) return x;
+                }
+                return x;
+            }
+
+
+        }
+        public void result(double n)
+        {
+            Inside inside = new Inside();
+            double result = inside.NewtonRaphson(n);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Klasy-Zagniezdzone/Klasy-Zagniezdzone/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Klasy_Zagniezdzone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Outside outside = new Outside();
+            outside.result(10);
+        }
+    }
+}
